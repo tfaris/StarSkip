dd3bd8a56fc396a677c077a92ab1b1aa0874e198
RA <jimjamhey@hotmail.com>
2019-11-11 03:59:24 +0000

    Asteroid Spawner. I was convinced I was going to try to do a pixel art style for this, but I want back to my comfort zone.
    It stays at the 0,0 square. So set the ship to 0,0,0 if you want to fly around with asteroids. They don't despawn. I'm not sure how we want to handle that.

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction { goUp, goDown, goRight, goLeft };
+
+public class Asteroid : MonoBehaviour
+{
+    public Direction goDirection;
+
+    public bool speedy = false;
+
+    float speed = 4;
+
+    public float spin;
+    public Rigidbody rb;
+
+    float rh;
+    float rv;
+
+    bool spinGo = true;
+    bool pushGo = true;
+
+    void Start()
+    {
+        rh = Random.Range(-.3f, .3f);
+        rv = Random.Range(-.3f, .3f);
+        rb = GetComponent<Rigidbody>();
+
+        speed = Random.Range(3,7);
+        if (speedy)
+        {
+            speed = speed * 2;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (spinGo == true)
+        {
+            addSpin();
+            spinGo = false;
+        }
+
+        if (pushGo == true)
+        {
+            if (goDirection == Direction.goUp)
+            {
+                addPush(Random.Range(-.8f,.8f), 1);
+            }
+            else if (goDirection == Direction.goDown)
+            {
+                addPush(Random.Range(-.8f, .8f), -1);
+            }
+            else if (goDirection == Direction.goRight)
+            {
+                addPush(1, Random.Range(-.8f, .8f));
+            }
+            else
+            {
+                addPush(-1, Random.Range(-.8f, .8f));
+            }
+            pushGo = false;
+        }
+    }
+
+    void addSpin()
+    {
+        rb.AddTorque(transform.up * rh, ForceMode.Impulse);
+        rb.AddTorque(transform.right * rv, ForceMode.Impulse);
+    }
+
+    void addPush(float pushx, float pushz)
+    {
+        rb.AddForce(new Vector3(pushx,0,pushz) * speed, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour
+{
+
+    public List<GameObject> asteroidPool = new List<GameObject>();
+
+    public Game game;
+
+    GameObject asteroidObject;
+    Asteroid asteroid;
+
+    public float spawnRate;
+    public float averageSpd;
+
+    public bool grey;
+    public bool larger;
+
+    public float xBoundary;
+    public float zBoundary;
+
+    public float weightedTwdsMid;
+
+    float timer;
+
+    Vector3 t;
+
+    void Start()
+    {
+        t = transform.position;
+    }
+
+    void Update()
+    {
+        xBoundary = game.gridWidth;
+        zBoundary = game.gridHeight;
+
+        timer += .2f * Time.deltaTime;
+
+        if (timer > spawnRate)
+        {
+            int dirDie = Random.Range(0,4);
+
+            if (dirDie == 0)
+            {
+                asteroidObject = Instantiate(asteroidPool[0], new Vector3(Random.Range(t.x, t.z + xBoundary), t.y, t.z), Random.rotation);
+                asteroid = asteroidObject.GetComponent<Asteroid>();
+                asteroid.goDirection = Direction.goUp;
+            }
+            else if (dirDie == 1)
+            {
+                asteroidObject = Instantiate(asteroidPool[0], new Vector3(Random.Range(t.x, t.x + xBoundary), t.y, t.z + zBoundary), Random.rotation);
+                asteroid = asteroidObject.GetComponent<Asteroid>();
+                asteroid.goDirection = Direction.goDown;
+            }
+            else if (dirDie == 2)
+            {
+                asteroidObject = Instantiate(asteroidPool[0], new Vector3(t.x + xBoundary, t.y, Random.Range(t.z, t.z + xBoundary)), Random.rotation);
+                asteroid = asteroidObject.GetComponent<Asteroid>();
+                asteroid.goDirection = Direction.goLeft;
+            }
+            else
+            {
+                asteroidObject = Instantiate(asteroidPool[0], new Vector3(t.x, t.y, Random.Range(t.z, t.z + xBoundary)), Random.rotation);
+                asteroid = asteroidObject.GetComponent<Asteroid>();
+                asteroid.goDirection = Direction.goRight;
+            }
+
+            timer = 0;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(t, new Vector3(t.x + xBoundary, t.y, t.z));
+        Gizmos.DrawLine(t, new Vector3(t.x, t.y, t.z + zBoundary));
+        Gizmos.DrawLine(new Vector3 (t.x, t.y, t.z + zBoundary), new Vector3(t.x + xBoundary, t.y, t.z + zBoundary));
+        Gizmos.DrawLine(new Vector3 (t.x + xBoundary, t.y, t.z), new Vector3(t.x + xBoundary, t.y, t.z + zBoundary));
+    }
+}
