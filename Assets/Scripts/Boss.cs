bfe47545b27f397b81291f2e4c1ebfc69947f523
RA <jimjamhey@hotmail.com>
2019-11-29 16:06:23 +0000

    Not a lot of time to add very much texture today, but I added a boss. It's a prefab. I put all the hurt boxes into a list and all the weak points as well. This is pretty low priority but it's there if you decide you want to mess around with it. It already rotates, but we could also have it wander around on the map slowly to add some difficulty

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+
+    public List<Collider> hurtBoxes = new List<Collider>();
+
+    public List<Collider> weakPoints = new List<Collider>();
+
+
+}
